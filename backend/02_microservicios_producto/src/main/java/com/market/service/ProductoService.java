37deeecf16ee8bfbd323d6cbd5093ff77f21ee45
TreviDev <javier.trevijano@outlook.com>
2024-08-12 18:40:23 +0200

    ProductoService and ProductoServiceImpl methods

---
+++ backend/02_microservicios_producto/src/main/java/com/market/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.market.service;
+
+import com.market.model.Categoria;
+import com.market.model.Producto;
+
+import java.util.List;
+
+public interface ProductoService {
+    List<Categoria> categorias();
+    List<Producto> productosPorcategoria(Integer idCategoria);
+    Producto actualizarStock(Integer idProducto, Integer unidades);
+    Producto productoPorCodigo(Integer idProducto);
+}
