dab2e574789b3d6f2f188e0783f1fa29c6c8d1e6
TreviDev <javier.trevijano@outlook.com>
2024-09-16 17:41:17 +0200

    Prueba Commit Categoria

--- frontend/src/app/model/Categoria.ts
+++ frontend/src/app/model/Categoria.ts
@@ -3,4 +3,10 @@ export class Categoria
     idCategoria:number;
     categoria:string;
     descripcion:string;
+    constructor()
+    {
+        this.idCategoria=0;
+        this.categoria="";
+        this.descripcion="";
+    }
 }
