04c9e4ed7cb818e073ad3be43ee25610e4473550
TreviDev <javier.trevijano@outlook.com>
2024-09-16 18:38:23 +0200

    Conectando con el EndPoint del Back

--- frontend/src/app/service/procesar-pedido.service.ts
+++ frontend/src/app/service/procesar-pedido.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProcesarPedidoService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
+
+  categorias():Observable<any> {
+    let url="http://localhost:8001/categorias";
+    return this.http.get(url);
+  }
 }
