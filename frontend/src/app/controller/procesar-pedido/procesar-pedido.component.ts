d3db9ef57dae5870757624007f91da5d4be8848b
TreviDev <javier.trevijano@outlook.com>
2024-09-21 11:00:38 +0200

    implementar función agregarProductoCarrito()

--- frontend/src/app/controller/procesar-pedido/procesar-pedido.component.ts
+++ frontend/src/app/controller/procesar-pedido/procesar-pedido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarritoCompra } from 'src/app/model/CarritoCompra';
 import { Categoria } from 'src/app/model/Categoria';
 import { Producto } from 'src/app/model/Producto';
 import { ProcesarPedidoService } from 'src/app/service/procesar-pedido.service';
@@ -13,7 +14,7 @@ export class ProcesarPedidoComponent implements OnInit {
   categorias:Categoria[]=[];
   productos:Producto[]=[];
   idCategoriasSel:number=0;
-
+  carrito:CarritoCompra[]=[];
   constructor(private procesarPedidoService: ProcesarPedidoService) { }
 
   ngOnInit(): void {
@@ -22,6 +23,15 @@ export class ProcesarPedidoComponent implements OnInit {
 
   productosCategoria(){
     this.procesarPedidoService.productosCategorias(this.idCategoriasSel).subscribe(p=>this.productos=p);
+  }
+
+  agregarProductoCarrito(producto:Producto){
+    if(producto.unidades<=producto.stock){
+      let item = new CarritoCompra();
+      item.producto=producto;
+      item.unidades=producto.unidades;
+      this.carrito.push(item);
+    }
   }
 
 }
