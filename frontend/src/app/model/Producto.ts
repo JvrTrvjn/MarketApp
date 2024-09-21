0e79ba59b13299b9aefc298b170426bd79beeabe
TreviDev <javier.trevijano@outlook.com>
2024-09-21 10:58:05 +0200

    Agregar nueva propiedad a Producto

--- frontend/src/app/model/Producto.ts
+++ frontend/src/app/model/Producto.ts
@@ -1,6 +1,7 @@
 export class Producto{
     idProducto:number;
     nombre:string;
+    unidades:number;
     precioUnitario:number;
     stock:number;
     idCategoria:number;
@@ -9,6 +10,7 @@ export class Producto{
     {
         this.idProducto=0;
         this.nombre="";
+        this.unidades=0;
         this.precioUnitario=0;
         this.stock=0;
         this.idCategoria=0;
