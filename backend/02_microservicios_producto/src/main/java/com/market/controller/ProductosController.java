b5c752472b10298471a19c802847fb76c0d8795e
TreviDev <javier.trevijano@outlook.com>
2024-09-21 22:18:44 +0200

    Implementar Get  EndPoint

--- backend/02_microservicios_producto/src/main/java/com/market/controller/ProductosController.java
+++ backend/02_microservicios_producto/src/main/java/com/market/controller/ProductosController.java
@@ -1,10 +1,28 @@
 package com.market.controller;
 
+import com.market.model.Categoria;
+import com.market.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 public class ProductosController {
-    Produc
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping(value = "categorias", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Categoria>> categorias(){
+        return new ResponseEntity<>(productoService.categorias(), HttpStatus.OK);
+    }
 }
