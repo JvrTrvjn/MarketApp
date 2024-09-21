6bbdcf770ebf9a2c0260e3abe1342e6ab898e1e5
TreviDev <javier.trevijano@outlook.com>
2024-09-21 22:24:46 +0200

    Implementar dos Get EndPoints más

--- backend/02_microservicios_producto/src/main/java/com/market/controller/ProductosController.java
+++ backend/02_microservicios_producto/src/main/java/com/market/controller/ProductosController.java
@@ -1,16 +1,14 @@
 package com.market.controller;
 
 import com.market.model.Categoria;
+import com.market.model.Producto;
 import com.market.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,15 @@ public class ProductosController {
     @GetMapping(value = "categorias", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Categoria>> categorias(){
         return new ResponseEntity<>(productoService.categorias(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "productos", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Producto>> productosCategoria(@RequestParam("idCategoria") int idCategoria){
+        return new ResponseEntity<>(productoService.productosPorCategoria(idCategoria), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "producto", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Producto> productoCodigo(@RequestParam("idProducto") int idProducto){
+        return new ResponseEntity<>(productoService.productoPorCodigo(idProducto), HttpStatus.OK);
     }
 }
